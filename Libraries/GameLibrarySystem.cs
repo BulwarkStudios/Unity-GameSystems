ec5a06a5f50fbe40eaae5d796f350085ba2f651d
Half <jeremy@bulwarkstudios.com>
2020-02-26 17:33:02 +0100

    Fix Project loading issues

--- Libraries/GameLibrarySystem.cs
+++ Libraries/GameLibrarySystem.cs
@@ -35,7 +35,7 @@ namespace BulwarkStudios.GameSystems.Libraries {
         /// <summary>
         /// Reset the initialisation
         /// </summary>
-        [InitializeOnLoadMethod]
+        [InitializeOnEnterPlayMode]
         static void OnProjectLoadedInEditor() {
             Instance.initialized = false;
             EditorApplication.playModeStateChanged -= PlayModeChanged;
